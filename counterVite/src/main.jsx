8f0167d49c2a4f02bae6acea8edc7dad56ee85d0
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-30 20:54:50 -0600

    Update main.jsx

--- counterVite/src/main.jsx
+++ counterVite/src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => {
-    return (<h1>Hola !</h1>);   
-}
+
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
