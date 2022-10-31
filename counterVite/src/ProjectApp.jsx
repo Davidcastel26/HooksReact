e366bcf7b30d2182d058b5f07c6636002d4d73d7
David Castellanos <davcastellanoslarios@gmail.com>
2022-10-30 21:06:38 -0600

    creating component

--- counterVite/src/ProjectApp.jsx
+++ counterVite/src/ProjectApp.jsx
@@ -1,7 +1,10 @@
+import FirstApp from './FirstApp'
+
 export const App = () => {
     return (
         <div>
             <h1>Hola perros</h1>
+            <FirstApp />
         </div>
     );   
 }
