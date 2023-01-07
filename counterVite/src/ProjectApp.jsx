89b32c5db3c64ce7602974d3fc6ef012eaf0d86a
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 13:17:01 -0600

    Revert "checking props"
    This reverts commit 3e1a9bc2a068b64243b701022bc56e2f05964c23.

--- counterVite/src/ProjectApp.jsx
+++ counterVite/src/ProjectApp.jsx
@@ -4,8 +4,8 @@ export const App = () => {
     return (
         <div>
             {/* <h1>Hola perros</h1> */}
-            {/* <FirstApp  title={dave}/> */}
-            <FirstApp title="hey this is dave!" subTitle={123}/>
+            <FirstApp />
+            {/* <FirstApp title="hey this is dave!" subTitle={123}/> */}
         </div>
     );   
 }
