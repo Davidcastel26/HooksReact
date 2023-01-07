911fd2b37b4b7b533cf453c46c955f3cce4a3536
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 13:42:07 -0600

    Create CounterApp.jsx

---
+++ counterVite/src/CounterApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const CounterApp = ({ value }) => {
+  return (
+    <>
+      <h2>{ value }</h2>
+    </>
+  )
+}
+
+CounterApp.propTypes = {
+  value: PropTypes.number.isRequired
+}
