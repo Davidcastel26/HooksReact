a998dfb1d102643db5f4f7da59e7ed4b5996d698
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 13:47:12 -0600

    Update main.jsx

--- counterVite/src/main.jsx
+++ counterVite/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './ProjectApp'
+// import { App } from './ProjectApp'
 
 import './styles.css'
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <App />
+        <CounterApp value={32}/>
     </React.StrictMode>
 )
