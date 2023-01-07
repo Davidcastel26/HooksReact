8c4907635d550be10efac20257d73c6cdcc02211
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 16:14:00 -0600

    Update CounterApp.jsx

--- counterVite/src/CounterApp.jsx
+++ counterVite/src/CounterApp.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+
+  const funtionForClick = (e) => {
+    console.log('hey')
+  }
+
   return (
     <>
-      <h1>Counter App</h1>
-      <h2>{ value }</h2>
-      <button> +1 </button>
+      <h1> Counter App </h1>
+      <h2> { value } </h2>
+      <button onClick={ funtionForClick } > +1 </button>
     </>
   )
 }
