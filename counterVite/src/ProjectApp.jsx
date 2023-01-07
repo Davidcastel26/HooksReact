aea131b9f53a630c6198c7b78ab57457979401ec
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-06 22:47:07 -0600

    Update ProjectApp.jsx

--- counterVite/src/ProjectApp.jsx
+++ counterVite/src/ProjectApp.jsx
@@ -4,7 +4,7 @@ export const App = () => {
     return (
         <div>
             {/* <h1>Hola perros</h1> */}
-            <FirstApp />
+            <FirstApp title="hey this is dave!" subTitle="124"/>
         </div>
     );   
 }
