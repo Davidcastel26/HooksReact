3e1a9bc2a068b64243b701022bc56e2f05964c23
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-06 23:07:19 -0600

    checking props

--- counterVite/src/FirstApp.jsx
+++ counterVite/src/FirstApp.jsx
@@ -1,35 +1,9 @@
-import React from 'react'
 
-// const newMessage = {
-//   message: 'Hola mundo',
-//   title: 'Fernando'
-// }
-// utilizar variables fijate que no cambian afuera del componente para que no se ester recargando esa info
-
-import PropTypes from 'prop-types';
-
-export const FirstApp = ({ title, subTitle }) => {
-  
-  // const something = (a , b) => {
-  //   return a + b;
-  // }
-  //Utilizar variables que son cambiantes dentro el componente
-  // if (!title){
-  //   throw new Error("the title does not exist")
-  // }
-  
+export const FirstApp = () => {
   return (
     <>
-      {/* <h1> { something(1,2) } </h1> */}
-      <h2>{ title }</h2>
-      {/* <p>Soy un subtitulo</p> */}
-      <p>{ subTitle + 1 }</p>
+      <div> FirstApp </div>
+
     </>
-  )
-}
-
-
-FirstApp.propTypes = {
-  title: PropTypes.string,
-
+    )
 }
