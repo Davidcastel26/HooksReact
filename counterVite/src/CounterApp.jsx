3c90bcadc747a3dfe7c463f1c85c5231c9ebeb56
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 17:04:53 -0600

    Update CounterApp.jsx

--- counterVite/src/CounterApp.jsx
+++ counterVite/src/CounterApp.jsx
@@ -7,17 +7,24 @@ export const CounterApp = ({ value }) => {
 
   const [ count, setCount ] = useState( value )
 
-  const funtionForClick = (e) => {
+  const Suma = (e) => {
     console.log('hey')
     // setCount( count + 1)
     setCount( (e)=> e + 1)
   }
 
+  const Resta = () => {
+    setCount( (e) => e - 1 )
+  }
+  const Reset = () => setCount(value)
+
   return (
     <>
       <h1> Counter App </h1>
       <h2> { count } </h2>
-      <button onClick={ funtionForClick } > +1 </button>
+      <button onClick={ Suma } > +1 </button>
+      <button onClick={ Resta }> -1 </button>
+      <button onClick={ Reset }> Reset </button>
     </>
   )
 }
