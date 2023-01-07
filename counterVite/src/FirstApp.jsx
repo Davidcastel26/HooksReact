b6c3f92599f4bca4b1a2375f865e6c8f5bbb61c2
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-06 22:56:18 -0600

    creating proptypes

--- counterVite/src/FirstApp.jsx
+++ counterVite/src/FirstApp.jsx
@@ -6,19 +6,26 @@ import React from 'react'
 // }
 // utilizar variables fijate que no cambian afuera del componente para que no se ester recargando esa info
 
+import PropTypes from 'prop-types';
+
 export const FirstApp = ({ title, subTitle }) => {
   
   // const something = (a , b) => {
   //   return a + b;
   // }
   //Utilizar variables que son cambiantes dentro el componente
-  
+
   return (
     <>
       {/* <h1> { something(1,2) } </h1> */}
       <h2>{ title }</h2>
       {/* <p>Soy un subtitulo</p> */}
-      <p>{ subTitle }</p>
+      <p>{ subTitle + 1 }</p>
     </>
   )
+}
+
+
+FirstApp.propTypes = {
+  
 }
