83e1da1841ed93e9e57067dd97d2bfc1d55972f0
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-06 22:27:21 -0600

    Update FirstApp.jsx

--- counterVite/src/FirstApp.jsx
+++ counterVite/src/FirstApp.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+const newMessage = {
+  message: 'Hola mundo',
+  title: 'Fernando'
+}
+
+const something = () => {
+  return 4+4;
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h1> Dave </h1>
+      <h1> { something() } </h1>
       <p>Soy un subtitulo</p>
     </>
   )
