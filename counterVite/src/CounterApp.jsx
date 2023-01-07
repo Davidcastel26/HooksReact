2aa1f463349d6715f3a9c8df6b610dae7a3a3ae9
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-07 16:27:21 -0600

    counter

--- counterVite/src/CounterApp.jsx
+++ counterVite/src/CounterApp.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { useState } from 'react'
+
 export const CounterApp = ({ value }) => {
+
+  const [ count, setCount ] = useState( value )
 
   const funtionForClick = (e) => {
     console.log('hey')
+    // setCount( count + 1)
+    setCount( (e)=> e + 1)
   }
 
   return (
     <>
       <h1> Counter App </h1>
-      <h2> { value } </h2>
+      <h2> { count } </h2>
       <button onClick={ funtionForClick } > +1 </button>
     </>
   )
