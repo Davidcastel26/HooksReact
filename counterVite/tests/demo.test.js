0a53df09ae2e63f3bb3a2b3fcdf0b38acbab272a
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-11 14:37:48 -0600

    getting jest updates

--- counterVite/tests/demo.test.js
+++ counterVite/tests/demo.test.js
@@ -1,6 +1,16 @@
 
 test('Esta prueba no puede fallar', () =>{
-    if (1 == 0 ){
-        throw new Error('No puedes dividir entre cero')
-    }
+    // if (1 == 0 ){
+    //     throw new Error('No puedes dividir entre cero')
+    // }
+
+    // 1. inicializacion
+    const message1 = "Hola mundo"
+
+    //2. estimulo 
+    const message2 = message1.trim()
+
+    // 3. observar el comportamiento... esperado
+    expect( message1 ).toBe( message2 )
+
 })
