e686eb7e2826a67804e6cff4eacc14cd78d87a9a
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-11 14:41:12 -0600

    Update demo.test.js

--- counterVite/tests/demo.test.js
+++ counterVite/tests/demo.test.js
@@ -1,16 +1,18 @@
 
-test('Esta prueba no puede fallar', () =>{
-    // if (1 == 0 ){
-    //     throw new Error('No puedes dividir entre cero')
-    // }
-
-    // 1. inicializacion
-    const message1 = "Hola mundo"
-
-    //2. estimulo 
-    const message2 = message1.trim()
-
-    // 3. observar el comportamiento... esperado
-    expect( message1 ).toBe( message2 )
-
+describe('Prebas en <DemoComponent />', ()=> {
+    test('Esta prueba no puede fallar', () =>{
+        // if (1 == 0 ){
+        //     throw new Error('No puedes dividir entre cero')
+        // }
+    
+        // 1. inicializacion
+        const message1 = "Hola mundo"
+    
+        //2. estimulo 
+        const message2 = message1.trim()
+    
+        // 3. observar el comportamiento... esperado
+        expect( message1 ).toBe( message2 )
+    
+    })
 })
