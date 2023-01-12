f7d60ad20db9961c322c7843038fd16055bdb558
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-11 20:25:27 -0600

    Update demo.test.js

--- counterVite/tests/demo.test.js
+++ counterVite/tests/demo.test.js
@@ -1,5 +1,6 @@
 
 describe('Prebas en <DemoComponent />', ()=> {
+    
     test('Esta prueba no puede fallar', () =>{
         // if (1 == 0 ){
         //     throw new Error('No puedes dividir entre cero')
