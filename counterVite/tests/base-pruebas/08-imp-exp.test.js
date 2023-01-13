c3b2f41fcbc6e234cfdced4c94e5b19bebfb005c
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-13 13:50:04 -0600

    Update 08-imp-exp.test.js

--- counterVite/tests/base-pruebas/08-imp-exp.test.js
+++ counterVite/tests/base-pruebas/08-imp-exp.test.js
@@ -1,11 +1,41 @@
-import { getHeroeById } from "../../src/base-pruebas/08-imp-exp";
+import { getHeroeById, getHeroesByOwner } from "../../src/base-pruebas/08-imp-exp";
 
 describe('testing 08-imp-exp', ()=> {
     
     test('getheroe by id should return a heroe by id', () =>{
         
         const id = 1;
-        const heroe = getHeroeById( id );
-        console.log(heroe);
+        const hero = getHeroeById( id );
+        // console.log(heroe);
+
+        expect( hero ).toEqual({ id: 1, name: 'Batman', owner: 'DC' })
+    })
+
+    test('getheroe by id should return undefined if does not exits', () =>{
+        
+        const id = 100;
+        const hero = getHeroeById( id );
+        // console.log(hero) //undefined
+
+        expect(hero).toBeFalsy()
+    })
+
+    test('should return an array from DCs heroes',()=>{
+
+        const owner = 'DC'
+        const heroes = getHeroesByOwner( owner )
+        console.log(heroes);
+
+        expect( heroes.length ).toBe(3)
+        expect(heroes).toEqual([
+            { id: 1, name: 'Batman', owner: 'DC' },
+            { id: 3, name: 'Superman', owner: 'DC' },
+            { id: 4, name: 'Flash', owner: 'DC' }
+          ])
+
+    })
+
+    test('should return Marvels heroes', () => {
+
     })
 })
