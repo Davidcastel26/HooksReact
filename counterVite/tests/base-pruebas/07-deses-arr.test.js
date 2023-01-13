c53412b5def723c65fcb744ea0b3ede9de6bf1b2
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-13 13:26:33 -0600

    Create 07-deses-arr.test.js

---
+++ counterVite/tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,16 @@
+import { retornaArreglo } from "../../src/base-pruebas/07-deses-arr"
+
+describe('testing 07-deses-arr', ()=> {
+    test('should return a string and a number', ()=> {
+        
+        const [ letters, numbers] = retornaArreglo()
+        // console.log(returnx);
+        expect( letters ).toBe( "ABC" )
+        expect( numbers ).toBe( 123 )
+
+        expect( typeof letters ).toBe('string')
+        expect( typeof numbers).toBe('number')
+
+        expect( letters ).toEqual( expect.any(String))
+    })
+})
