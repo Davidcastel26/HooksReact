90f449ae2feb002d20e1dbf95f35bf23b4099d86
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-13 11:58:45 -0600

    Update 02-template-string.test.js

--- counterVite/tests/base-pruebas/02-template-string.test.js
+++ counterVite/tests/base-pruebas/02-template-string.test.js
@@ -1,7 +1,13 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
 
 describe('Pruebas en 02-template-string', () => {
     
     test('should getSaludo return "hello Dave"', ()=>{
+
+        const name = 'Davd';
+        const message = getSaludo( name );
+
+        expect( message ).toBe( ` Hola ${ name } `)
 
     })
 })
