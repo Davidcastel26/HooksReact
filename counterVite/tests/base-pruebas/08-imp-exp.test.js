f84e427ef53f26eb43abfcb2cb8a1d9b0ce8dccd
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-13 13:53:17 -0600

    Update 08-imp-exp.test.js

--- counterVite/tests/base-pruebas/08-imp-exp.test.js
+++ counterVite/tests/base-pruebas/08-imp-exp.test.js
@@ -1,4 +1,5 @@
 import { getHeroeById, getHeroesByOwner } from "../../src/base-pruebas/08-imp-exp";
+import heroes from "../../src/data/heroes";
 
 describe('testing 08-imp-exp', ()=> {
     
@@ -23,15 +24,16 @@ describe('testing 08-imp-exp', ()=> {
     test('should return an array from DCs heroes',()=>{
 
         const owner = 'DC'
-        const heroes = getHeroesByOwner( owner )
-        console.log(heroes);
+        const heroess = getHeroesByOwner( owner )
+        console.log(heroess);
 
-        expect( heroes.length ).toBe(3)
-        expect(heroes).toEqual([
+        expect( heroess.length ).toBe(3)
+        expect(heroess).toEqual([
             { id: 1, name: 'Batman', owner: 'DC' },
             { id: 3, name: 'Superman', owner: 'DC' },
             { id: 4, name: 'Flash', owner: 'DC' }
           ])
+        expect( heroess ).toEqual( heroes.filter( (heroe) => heroe.owner === owner) )
 
     })
 
