43c91a478ce635571d13de4cb84c44c8dbb7426e
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-13 13:04:53 -0600

    getting start with the test

---
+++ counterVite/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,12 @@
+
+describe('testing into 05-funciones', () => {
+
+    test('getUser should return an object', ()=> {
+
+        const testUser = {
+            uid: 'ABC123',
+            username: 'El_Papi1502'
+    }
+
+    })
+})
