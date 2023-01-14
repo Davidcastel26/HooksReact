8b2f7c6316dc75402eea9061bb688f6dc7583284
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-14 15:26:08 -0600

    Update 09-promesas.js

--- counterVite/src/base-pruebas/09-promesas.js
+++ counterVite/src/base-pruebas/09-promesas.js
@@ -7,8 +7,6 @@ export const getHeroeByIdAsync = ( id ) => {
     return new Promise( (resolve, reject) => {
 
         setTimeout( () =>  {
-            // Tarea
-            // importen el 
             const p1 = getHeroeById( id );
             if ( p1 ) {
                 resolve( p1 );
