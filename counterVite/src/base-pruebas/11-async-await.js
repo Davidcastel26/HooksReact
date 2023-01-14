427876f56f9ba017813c26ac4e0f3272e4a43d4f
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-14 16:05:55 -0600

    Update 11-async-await.js

--- counterVite/src/base-pruebas/11-async-await.js
+++ counterVite/src/base-pruebas/11-async-await.js
@@ -13,13 +13,12 @@ export const getImagen = async() => {
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        return url
 
     } catch (error) {
         // manejo del error
         console.error(error)
+        return 'No se encontro la imagen'
     }
     
     
