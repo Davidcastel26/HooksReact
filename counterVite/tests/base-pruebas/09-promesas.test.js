e47b346793fdeb2bfe6fb190934b3040d5acf825
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-14 15:48:29 -0600

    Update 09-promesas.test.js

--- counterVite/tests/base-pruebas/09-promesas.test.js
+++ counterVite/tests/base-pruebas/09-promesas.test.js
@@ -2,9 +2,34 @@ import { getHeroeByIdAsync } from "../../src/base-pruebas/09-promesas";
 
 describe('testing 09-promesas', () => {
     
-    test('getHeroByIdAsync should return an hero', ()=> {
+    test('getHeroByIdAsync should return an hero', ( done ) => {
 
         const id = 1;
         getHeroeByIdAsync( id )
+            .then( hero => {
+
+                expect( hero ).toEqual( {
+                    id:1,
+                    name:'Batman',
+                    owner:'DC'
+                } )
+
+                done()
+            })
+    })
+    test('getHeroByIdAsync should return an error if hero does not exist', ( done ) => {
+
+        const id = 1;
+        getHeroeByIdAsync( id )
+            .then( hero => {
+
+                expect( hero ).toEqual( {
+                    id:1,
+                    name:'Batman',
+                    owner:'DC'
+                } )
+
+                done()
+            })
     })
 })
