758b55c0ad17fde28921219f625cdf875e2369c7
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-14 18:58:16 -0600

    Create FirstApp.test.jsx

---
+++ counterVite/tests/FirstApp.test.jsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import { FirstApp } from "../src/FirstApp"
+
+describe('testing component <FirstApp/>', () => { 
+    
+    test('should be the same as the snapshot', () => {
+
+        render( <FirstApp/>)
+    })
+
+ })
