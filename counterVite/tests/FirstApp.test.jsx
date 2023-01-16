ed85eb45ede2083d5a2cb5bce97f9411216ceb8f
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-16 12:41:15 -0600

    Update FirstApp.test.jsx

--- counterVite/tests/FirstApp.test.jsx
+++ counterVite/tests/FirstApp.test.jsx
@@ -6,7 +6,7 @@ describe('testing component <FirstApp/>', () => {
     test('should be the same as the snapshot', () => {
 
         const title = "Hola soy yo";
-        render(<FirstApp title={title}/>)
+        render( < FirstApp title={title}/ > )
     })
 })
         
