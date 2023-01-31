ac3955a9a90da53a1c0fa3893090f8dc25af2697
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-31 17:55:00 -0600

    working in the simpleForm component

--- hook-app/src/02-useEffect/SimpleForm.jsx
+++ hook-app/src/02-useEffect/SimpleForm.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react"
+
+const infoForm = {
+    username: 'strider',
+    email: 'david@google.com'
+}
 
 export default function SimpleForm() {
+
+    const [formState, setFormState] = useState(infoForm)
+
+    const { username, email } = formState;
+
+    const onInputChange = ( {target} ) => {
+        // console.log(event.target.name);
+        const { name, value} = target;
+        setFormState({...formState, [ name ]: value})
+    }
+
+    
+
   return (
     <>
         <h1> SimpleForm </h1>
+        <hr />
+
+        <input 
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={ onInputChange }
+        />
+        <input 
+            type="text"
+            className="form-control mt-2"
+            placeholder="david@google.com"
+            name="email"
+            value={ email }
+            onChange={ onInputChange }
+        />
     </>
   )
 }
