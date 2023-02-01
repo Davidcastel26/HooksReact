1960b496b1b1346cf7ca4c0a4bf106da9026d6a6
David Castellanos <davcastellanoslarios@gmail.com>
2023-01-31 18:51:49 -0600

    Update Messege.jsx

--- hook-app/src/02-useEffect/Messege.jsx
+++ hook-app/src/02-useEffect/Messege.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react"
 
 export default function Messege() {
+
+    const [coords, setCoords] = useState( { x:0, y:0} )
+
+    useEffect(() => {
+        
+        const onMouseMove = ( {x, y} ) =>{
+            // const coords = { x, y}
+            // console.log(coords);
+            setCoords( { x, y})
+        }
+
+        window.addEventListener('mousemove', onMouseMove )
+    
+      return () => {
+        window.removeEventListener('mousemove', onMouseMove)
+      }
+    }, [])
+    
+
   return (
     <>
         <h3>Messege usuario existe</h3>
+        { JSON.stringify( coords )}
     </>
   )
 }
