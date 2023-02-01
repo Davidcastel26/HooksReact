9fdeb3a63a6621ba029994ce09a9d4617e9f907a
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-01 17:08:19 -0600

    Update useForm.js

--- hook-app/src/hooks/useForm.js
+++ hook-app/src/hooks/useForm.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 
 export const useForm = () => {
 
-    
+    const infoForm = {
+        username: '',
+        email: '',
+        password: ''
+    }
+
+    const [formState, setFormState] = useState(infoForm)
+
+    const { username, email, password } = formState;
+
+    const onInputChange = ( {target} ) => {
+        // console.log(event.target.name);
+        const { name, value} = target;
+        setFormState({...formState, [ name ]: value})
+    }
 
     return{
 
