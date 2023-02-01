add7273dc3b4fc76af02863050f53b9effc8d5d8
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-01 17:31:21 -0600

    Update main.jsx

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -6,10 +6,11 @@ import './index.css'
 // import CounterApp from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import SimpleForm from './02-useEffect/SimpleForm'
-import { FomrWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { FomrWithCustomHook } from './02-useEffect/FormWithCustomHook'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <FomrWithCustomHook />
+    <MultipleCustomHooks />
   // </React.StrictMode>,
 )
