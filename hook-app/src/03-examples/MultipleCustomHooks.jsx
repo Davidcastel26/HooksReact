f7ed78de0910b56d1da1f5875a967bdfac1f9264
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-02 17:01:11 -0600

    Update MultipleCustomHooks.jsx

--- hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,14 @@
+import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
+
+  const {} = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes`)
+
   return (
     <>
         <h1>Breaking Bad Quotes</h1>  
         <hr />
+
     </>
   )
 }
