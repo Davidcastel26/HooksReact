468a5bd6c700c8c44c8cc457a2b31e60504da038
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-04 22:33:00 -0600

    Create Quote.jsx

---
+++ hook-app/src/03-examples/Quote.jsx
@@ -0,0 +1,18 @@
+
+
+export const Quote = () => {
+  return (
+    <div>
+
+        <blockquote className="blockquote text-end">
+            <p className="mb-1"> { quote } </p>
+            <footer className="blockquote-footer"> { author } </footer>
+        </blockquote>
+        <hr />
+        <blockquote className="blockquote text-center">
+            <p className="mb-1"> { name } </p>
+            <footer className="blockquote-footer"> { species } </footer>
+        </blockquote>
+    </div>
+  )
+}
