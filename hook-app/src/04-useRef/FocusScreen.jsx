18705a64456432d5b9548c563e461619454db84e
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-05 09:55:38 -0600

    Create FocusScreen.jsx

---
+++ hook-app/src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,11 @@
+
+export const FocusScreen = () => {
+  return (
+    <>
+        <h1>Focus Screen</h1>
+        <hr />
+
+        <input type="text" placeholder="Ingrese su number" />
+    </>
+  )
+}
