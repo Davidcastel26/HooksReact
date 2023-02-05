f86285443e1e9cd718f85de0d53930378ddae058
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-05 12:33:13 -0600

    Update main.jsx

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -10,10 +10,11 @@ import './index.css'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { MemorizeComp } from './06-memos/MemorizeComp'
+// import { MemorizeComp } from './06-memos/MemorizeComp'
+import { MemoHook } from './06-memos/MemoHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MemorizeComp />
+    <MemoHook />
   // </React.StrictMode>,
 )
