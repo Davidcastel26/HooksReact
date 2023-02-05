1cb3a56af73647fc9f2a0ba91f6db8ddb943e223
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-05 10:09:04 -0600

    Update FocusScreen.jsx

--- hook-app/src/04-useRef/FocusScreen.jsx
+++ hook-app/src/04-useRef/FocusScreen.jsx
@@ -1,17 +1,27 @@
+import { useRef } from "react"
 
 export const FocusScreen = () => {
+
+    const inputRef = useRef()
+
+    const onClick = () => {
+        // console.log(inputRef);
+        inputRef.current.select();
+    }
+
   return (
     <>
         <h1>Focus Screen</h1>
         <hr />
 
         <input 
+            ref={ inputRef }
             type="text" 
             placeholder="Ingrese su number" 
             className="form-control" 
         />
 
-        <button className="btn btn-primary mt-2">
+        <button className="btn btn-primary mt-2" onClick={onClick}>
             Set Focus
         </button>
     </>
