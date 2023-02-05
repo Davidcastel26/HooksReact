17058da30b6d12c6b80e476fd4f1bfffce19af77
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-04 22:33:46 -0600

    Update LoadingQuote.jsx

--- hook-app/src/03-examples/LoadingQuote.jsx
+++ hook-app/src/03-examples/LoadingQuote.jsx
@@ -1,18 +1,11 @@
 
-export const LoadingQuote = (author, quote, name, species ) => {
+export const LoadingQuote = ( isLoading ) => {
 
     return(
-        <div>
-
-            <blockquote className="blockquote text-end">
-                <p className="mb-1"> { quote } </p>
-                <footer className="blockquote-footer"> { author } </footer>
-            </blockquote>
-            <hr />
-            <blockquote className="blockquote text-center">
-                <p className="mb-1"> { name } </p>
-                <footer className="blockquote-footer"> { species } </footer>
-            </blockquote>
+        <div className="alert alert-info text-center">
+        Loading...
         </div>
     )
 }
+
+
