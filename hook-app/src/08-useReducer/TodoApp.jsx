53d83c37e5721e03c5ce6823a85cbe98098697fe
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-07 17:46:52 -0600

    Update TodoApp.jsx

--- hook-app/src/08-useReducer/TodoApp.jsx
+++ hook-app/src/08-useReducer/TodoApp.jsx
@@ -53,14 +53,18 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    <TodoList todos={todos} onDeleteTodo={ handleDeleteTodo } onToggleTodo={ handleToggleTodo } />
+                    <TodoList 
+                        todos={todos} 
+                        onDeleteTodo={ handleDeleteTodo } 
+                        onToggleTodo={ handleToggleTodo }
+                    />
                 </div>
 
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
                     
-                    <FormTodo onNewTodo={ handleNewTodo  }/>
+                    <FormTodo onNewTodo={ handleNewTodo }/>
                     
                 </div>
             </div>
