28da99f23bd934db3381e2190c19e7ccc5a49c75
David Castellanos <davcastellanoslarios@gmail.com>
2023-02-13 18:57:12 -0600

    Update MainApp.jsx

--- hook-app/src/09-useContext/MainApp.jsx
+++ hook-app/src/09-useContext/MainApp.jsx
@@ -1,9 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import { HomePage } from "./HomePage"
+import { LoginPage } from "./LoginPage"
+import { AboutPage } from "./AboutPage"
 
 export const MainApp = () => {
   return (
     <>
         <h1>Main App</h1>
         <hr />
+
+        <Routes>
+            <Route path="/" element={ <HomePage /> }/>
+            <Route path="about" element={ <AboutPage /> }/>
+            <Route path="login" element={ <LoginPage /> }/>
+            <Route path="/*" element={<ErrorPage />}
+        </Routes>
     </>
   )
 }
