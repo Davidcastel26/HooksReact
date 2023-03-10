1d06168254367d834fd3978320b7fb12b5195e00
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-09 23:29:23 -0600

    nav

--- hook-app/src/09-useContext/Navbar.jsx
+++ hook-app/src/09-useContext/Navbar.jsx
@@ -1,23 +1,29 @@
 import { Link, NavLink } from "react-router-dom"
 
 
-export const Navbar = () => {
-        return (
+export const Navbar = () => { 
+    
+    
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-      
-      <Link className="navbar-brand" to="/">UseContext</Link>
-      
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          
-            <NavLink to="/" className={()=> `nav-link ${ isActive ? 'active' : '' }`} > Home </NavLink>
-            <NavLink to="/about" className={()=> `nav-link ${ isActive ? 'active' : '' }`} > About </NavLink>
-            <NavLink to="/login" className={()=> `nav-link ${ isActive ? 'active' : '' }`} > Login </NavLink>
+  <div className="container-fluid">
+
+<Link className="navbar-brand" to="/">UseContext</Link>
+
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
         
-        </ul>
-      </div>
+        <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active' : ''}` } to="/">Home</NavLink>
+        <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active' : ''}` } to="/about">About</NavLink>
+        <NavLink className={ ({isActive}) => `nav-link ${ isActive ? 'active' : ''}` } to="/login">Login</NavLink>
+
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
   )
 }
