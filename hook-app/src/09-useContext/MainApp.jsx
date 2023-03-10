10116c9fa2388b44529935963d1938c6439eb29a
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-09 23:47:51 -0600

    user context and provider

--- hook-app/src/09-useContext/MainApp.jsx
+++ hook-app/src/09-useContext/MainApp.jsx
@@ -4,10 +4,11 @@ import { LoginPage } from "./LoginPage"
 import { AboutPage } from "./AboutPage"
 import { ErrorPage } from "./ErrorPage"
 import { Navbar } from "./Navbar"
+import { UserProvider } from "./context/UserProvider"
 
 export const MainApp = () => {
   return (
-    <>
+    <UserProvider>
         <Navbar/>
         <hr />
 
@@ -19,6 +20,6 @@ export const MainApp = () => {
             <Route path="/*" element={<ErrorPage />} />
             <Route path="/cv" element={ <Navigate to="/about" />} />
         </Routes>
-    </>
+    </UserProvider>
   )
 }
