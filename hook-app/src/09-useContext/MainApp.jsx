af5aacc2c213337cdc221a916404b67c202973fe
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-09 21:54:45 -0600

    working with link

--- hook-app/src/09-useContext/MainApp.jsx
+++ hook-app/src/09-useContext/MainApp.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate, Link } from "react-router-dom"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
 import { AboutPage } from "./AboutPage"
@@ -8,6 +8,11 @@ export const MainApp = () => {
   return (
     <>
         <h1>Main App</h1>
+
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+
         <hr />
 
         <Routes>
