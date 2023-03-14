884938bf7d2c5fcda9bedfaaf405eff03069f8b8
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-13 19:57:35 -0600

    Update UserProvider.jsx

--- hook-app/src/09-useContext/context/UserProvider.jsx
+++ hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-    id:123,
-    name: 'dave',
-    mail:'david@mail.com'
-}
+// const user = {
+//     id:123,
+//     name: 'dave',
+//     mail:'david@mail.com'
+// }
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState()
+
   return (
-    <UserContext.Provider value={{ hola:'Mundo', user}}>
+    // <UserContext.Provider value={{ hola:'Mundo', user}}>
+    <UserContext.Provider value={{ user }}>
         { children }
     </UserContext.Provider>
   )
