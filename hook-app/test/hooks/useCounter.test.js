a0103a86c13a0a59c539d490be951725c53f3fce
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-13 22:35:37 -0600

    Update useCounter.test.js

--- hook-app/test/hooks/useCounter.test.js
+++ hook-app/test/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-const { renderHook } = require("@testing-library/react")
+const { renderHook, render } = require("@testing-library/react")
 const { useCounter } = require("../../src/hooks/useCounter")
 
 describe('testing into useCounter', () => {
@@ -6,7 +6,21 @@ describe('testing into useCounter', () => {
     test('should return the values by default', () => {
 
         const { result } = renderHook(() => useCounter());
-        console.log(result);
+        // console.log(result);
+        const { counter, decrement, increment, reset } = result.current;
+
+        expect(counter).toBe(10)
+        expect(decrement).toEqual( expect.any(Function) )
+        expect(increment).toEqual( expect.any(Function))
+        expect(reset).toEqual(expect.any(Function))
+    })
+
+    test('should return in the counter a value of 100', () => {
+
+        const {result} = renderHook( () => useCounter(100))
+        
+        expect( result.current.counter).toBe(100)
+
     })
 
 })
