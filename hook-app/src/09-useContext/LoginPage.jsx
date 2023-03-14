a05e6f75afa1a08a2ba12384cdc3eafcfee4897c
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-13 19:29:54 -0600

    Update LoginPage.jsx

--- hook-app/src/09-useContext/LoginPage.jsx
+++ hook-app/src/09-useContext/LoginPage.jsx
@@ -1,6 +1,18 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
 
 export const LoginPage = () => {
+
+  const { user} = useContext( UserContext )
+  // console.log(hola);
+
   return (
+    <>
     <div>LoginPage</div>
+    <hr />
+    <pre>
+      { JSON.stringify(user, null, 3)}
+    </pre>
+    </>
   )
 }
