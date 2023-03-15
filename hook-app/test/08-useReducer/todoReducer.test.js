94efc337d514a15876ccdde9df24e1739aa9c761
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-14 21:37:18 -0600

    Create todoReducer.test.js

---
+++ hook-app/test/08-useReducer/todoReducer.test.js
@@ -0,0 +1,16 @@
+
+describe('testing into TodoReducer', () => { 
+
+    const initialState = [{
+        id:1,
+        description:'Demo Todo',
+        done: false
+    }]
+    
+    test('should return initial state', () => { 
+        
+
+
+     })
+    
+ })
