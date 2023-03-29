543922df98a9fe7e4156c24778743cdffb005aff
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-28 19:28:46 -0600

    Update TodoItem.test.jsx

--- hook-app/test/08-useReducer/TodoItem.test.jsx
+++ hook-app/test/08-useReducer/TodoItem.test.jsx
@@ -1,12 +1,23 @@
+import { render } from "@testing-library/react";
+import { TodoItem } from "../../src/08-useReducer/TodoItem";
+
 describe('test into <TodoItem>', () => { 
 
     const todo = {
         id:1,
-        description: 'Piedra del Alma'
-    }
+        description: 'Piedra del Alma',
+        done: false
+    };
     
+    const onDeleteTodoMock = jest.fn();
+    const onToggleTodoMock = jest.fn();
+
+    beforeEach( () => jest.clearAllMocks())
+
     test('should show the pendding to do', () => { 
         
+        render( <TodoItem todo={ todo } onToggleTodo={onToggleTodoMock} onDeleteTodo={ onDeleteTodoMock }/>)
+
     })
 
  })
