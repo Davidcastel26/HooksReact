076969921ff1dc1637cb57d9055fb9edf1059eaa
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-28 19:18:39 -0600

    Create TodoItem.test.jsx

---
+++ hook-app/test/08-useReducer/TodoItem.test.jsx
@@ -0,0 +1,12 @@
+describe('test into <TodoItem>', () => { 
+
+    const todo = {
+        id:1,
+        description: 'Piedra del Alma'
+    }
+    
+    test('should show the pendding to do', () => { 
+        
+    })
+
+ })
