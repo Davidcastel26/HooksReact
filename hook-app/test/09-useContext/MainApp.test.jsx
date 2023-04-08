50aa6357410a555177e01ff385419ecdae04e7d8
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-07 23:59:31 -0600

    testing <MainApp />

---
+++ hook-app/test/09-useContext/MainApp.test.jsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { MainApp } from "../../src/09-useContext/MainApp";
+import { MemoryRouter } from "react-router-dom";
+
+describe('testing <MainApp />', () => { 
+
+    test('should show the home page', () => { 
+        
+        render(
+            <MemoryRouter>
+                <MainApp /> 
+            </MemoryRouter>
+        )
+
+        expect( screen.getByText("HomePage") ).toBeTruthy()
+
+    })
+
+    test('should show login pag', () => { 
+
+        render(
+            <MemoryRouter initialEntries={['/login']}>
+                <MainApp />
+            </MemoryRouter>
+        )
+
+        expect( screen.getByText("LoginPage") ).toBeTruthy()
+        // screen.debug()
+
+    })
+
+});
