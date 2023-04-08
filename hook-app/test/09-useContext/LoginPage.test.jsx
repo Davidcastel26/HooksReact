3ddc054f4826ca537729fa344d52ff7a558c68fb
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-07 23:39:09 -0600

    Update LoginPage.test.jsx

--- hook-app/test/09-useContext/LoginPage.test.jsx
+++ hook-app/test/09-useContext/LoginPage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { UserContext } from "../../src/09-useContext/context/UserContext"
 import { LoginPage } from "../../src/09-useContext/LoginPage"
 
@@ -13,13 +13,25 @@ describe('testing into <LoginPage/>', () => {
         )
 
         const preTag = screen.getByLabelText('pre');
-        
+
         expect( preTag.innerHTML ).toBe('null')
 
     })
 
     test('should call the setUser when click in the btn ', () => { 
         
+        const setUserMock = jest.fn()
+
+        render(
+            <UserContext.Provider value={{user: null, setUser: setUserMock}}>
+                <LoginPage />
+            </UserContext.Provider>
+        );
+
+        const btn = screen.getByRole('button');
+        fireEvent.click(btn)
+
+        expect( setUserMock ).toHaveBeenCalledWith()
 
     })
  })
