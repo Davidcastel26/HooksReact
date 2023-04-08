ebd4aed132315f7d57715f8c3c6f27f07d3bdea3
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-07 23:41:26 -0600

    Update LoginPage.jsx

--- hook-app/src/09-useContext/LoginPage.jsx
+++ hook-app/src/09-useContext/LoginPage.jsx
@@ -3,7 +3,7 @@ import { UserContext } from "./context/UserContext";
 
 export const LoginPage = () => {
 
-  const { user} = useContext( UserContext )
+  const { user, setUser} = useContext( UserContext )
   // console.log(hola);
 
   return (
@@ -13,6 +13,11 @@ export const LoginPage = () => {
     <pre aria-label="pre">
       { JSON.stringify(user, null, 3)}
     </pre>
+
+    <button className="btn btn-primary" onClick={() => setUser({id:123, name:'Juan', email:'juan@google.com'})}>
+      Establecer Usuario
+    </button>
+
     </>
   )
 }
