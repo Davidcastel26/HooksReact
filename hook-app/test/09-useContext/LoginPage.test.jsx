9428686911963812704e4a4fac623d1446c833f6
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-07 23:31:05 -0600

    Create LoginPage.test.jsx

---
+++ hook-app/test/09-useContext/LoginPage.test.jsx
@@ -0,0 +1,11 @@
+describe('testing into <LoginPage/>', () => { 
+    
+    test('should show the component with out users', () => { 
+        
+    })
+
+    test('should call the setUser when click in the btn ', () => { 
+        
+
+    })
+ })
