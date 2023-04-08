d339f578a2cc1aeb3eca1ba95cf972ea2f694eab
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-07 23:33:29 -0600

    should show the component with out users

--- hook-app/test/09-useContext/LoginPage.test.jsx
+++ hook-app/test/09-useContext/LoginPage.test.jsx
@@ -1,7 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { UserContext } from "../../src/09-useContext/context/UserContext"
+import { LoginPage } from "../../src/09-useContext/LoginPage"
+
 describe('testing into <LoginPage/>', () => { 
     
     test('should show the component with out users', () => { 
         
+        render(
+            <UserContext.Provider value={{ user: null}}>
+                <LoginPage />
+            </UserContext.Provider>
+        )
+
+        const preTag = screen.getByLabelText('pre')
+
     })
 
     test('should call the setUser when click in the btn ', () => { 
